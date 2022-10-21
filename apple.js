d78bf31aa20f7ff8db5cf0ed8a33375e10cd757a
Fermain <Fermain@users.noreply.github.com>
2022-10-21 16:18:40 +0200

    e2e lesson

--- apple.js
+++
@@ -1,6 +0,0 @@
-export default {
-  color: "red",
-  texture: "juicy",
-  isVegan: true,
-  rating: 4.5
-}
---
+++ cypress.config.js
@@ -0,0 +1,22 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  reporter: "cypress-mochawesome-reporter",
+  reporterOptions: {
+    reportDir: 'cypress/reports',
+    html: true,
+    json: true,
+    charts: true,
+    inlineAssets: true,
+    overwrite: false,
+    autoOpen: true,
+    cdn: true
+  },
+  video: false,
+  retries: 1,
+  e2e: {
+    setupNodeEvents(on, config) {
+      require("cypress-mochawesome-reporter/plugin")(on);
+    },
+  },
+});
