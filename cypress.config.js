5ddd76cfaae5aad05cef8f6cb341baa7d263148e
Fermain <Fermain@users.noreply.github.com>
2022-10-21 17:41:53 +0200

    removes reporting

--- cypress.config.js
+++ cypress.config.js
@@ -1,22 +1,9 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  reporter: "cypress-mochawesome-reporter",
-  reporterOptions: {
-    reportDir: 'cypress/reports',
-    html: true,
-    json: true,
-    charts: true,
-    inlineAssets: true,
-    overwrite: false,
-    autoOpen: true,
-    cdn: true
-  },
-  video: false,
-  retries: 1,
   e2e: {
     setupNodeEvents(on, config) {
-      require("cypress-mochawesome-reporter/plugin")(on);
+      // implement node event listeners here
     },
   },
 });
